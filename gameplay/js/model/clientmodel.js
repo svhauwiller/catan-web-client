8b3f32d86ec503bab50aa104273f5a87abad61be
whauwiller <whauwiller@gmail.com>
2014-01-31 13:11:20 -0700

    Added pseudocode for each method in client model

--- gameplay/js/model/clientmodel.js
+++ gameplay/js/model/clientmodel.js
@@ -87,11 +87,11 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* @type {ElemType int}
 		*/
 		function ClientModel(playerID){
-			this.deck = new DevCardList();
-			this.bank = new ResourceList();
+			this.bank = new Bank();
 			this.biggestArmy=null;
 			this.biggestArmySize=0;
 			this.chat = new Array();
+			this.devCard = new DevCard();
 			this.log = new Array();
 			this.longestRoad = null;
 			this.longestRoadSize = 0;
@@ -128,7 +128,11 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* @param {ResourceList} offerList list of resources being offered
 		* @param {ResourceList} requestList list of resources being requested
 		*/
-		ClientModel.prototype.acceptTrade = function(offerList, requestList) {}
+		ClientModel.prototype.acceptTrade = function(offerList, requestList) {
+			//1. Remove the resources requested
+			//2. Add the resources offered
+			//3. Ping the other player as accepted
+		}
 
 		/**
 		* Discard a list of cards from player's hand
@@ -141,7 +145,10 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* @method discardCards
 		* @params {ResourceList} listToDiscard list of resource cards that are to be discarded
 		*/	
-		ClientModel.prototype.discardCards = function(listToDiscard) {}
+		ClientModel.prototype.discardCards = function(listToDiscard) {
+			//1 Decrement the selected cards from player's hand
+			//2 Add selected cards to bank
+		}
 		
 		/**
 		* Send a message to the chat log
@@ -154,7 +161,9 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* @method chat
 		* @param {String} chatLine line of chat to send
 		*/
-		ClientModel.prototype.chat = function(chatLine) {}
+		ClientModel.prototype.chat = function(chatLine) {
+			//1 Send line of chat to the server
+		}
 		
 		/**
 		* Randomly determine the result of the die roll to distribute resources
@@ -166,7 +175,17 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		*
 		* @method rollDice
 		*/
-		ClientModel.prototype.rollDice = function() {}
+		ClientModel.prototype.rollDice = function() {
+			//1 Randomly select roll 2 dice
+			//2 Is Seven
+				//2a Force Discard
+				//2b Move robber
+				//2c Steal Resources
+			//3 Is not Seven
+				//3a Look for all hexes with that number
+				//3b Look for all settlements and cities by the hex and find owner
+				//3c Award resources to players
+		}
 
 		/**
 		* Build a road at a specified edge
@@ -177,9 +196,15 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* </pre>
 		*
 		* @method buildRoad
-		* @param {Edge} edgeToBuild the edge to build the road upon
+		* @param {Hex} hex Hex where the edgeis based from
+		* @param {string} edgeDirection the direction the edge is at from the hex
 		*/
-		ClientModel.prototype.buildRoad = function(edgeToBuild) {}
+		ClientModel.prototype.buildRoad = function(hex, vertDirection) {
+			//1 Confirm the that player has the right resources
+			//2 Confirm that the edge is a valid building location
+			//3 Reduce the player's resources
+			//4 Update the map to have the road on the edge
+		}
 
 		/**
 		* Build a settlement on a specified vertex
@@ -190,9 +215,15 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* </pre>
 		*
 		* @method buildSettlement
-		* @param {Vertex} vertexToBuild the vertex to build the edge upon
+		* @param {Hex} hex Hex where the vertex is based from
+		* @param {string} vertDirection the direction the vertex is at from the hex
 		*/
-		ClientModel.prototype.buildSettlement = function(vertexToBuild) {}
+		ClientModel.prototype.buildSettlement = function(hex, vertDirection) {
+			//1 Confirm the that player has the right resources
+			//2 Confirm that the vertex is a valid building location
+			//3 Reduce the player's resources
+			//4 Update the map to have the road on the vertex
+		}
 
 		/**
 		* Build a city on a specifed vertex
@@ -203,9 +234,15 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* </pre>
 		*
 		* @method buildCity
-		* @param {Vertex} vertexToBuild the vertex to build the edge upon
+		* @param {Hex} hex Hex where the vertex is based from
+		* @param {string} vertDirection the direction the vertex is at from the hex
 		*/
-		ClientModel.prototype.buildCity = function(vertexToBuild) {}
+		ClientModel.prototype.buildCity = function(hex, vertDirection) {
+			//1 Confirm the that player has the right resources
+			//2 Confirm that the vertex is a valid building location
+			//3 Reduce the player's resources
+			//4 Update the map to have the road on the vertex
+		}
 
 		/**
 		* Buy a Development card for the player's hand
@@ -216,7 +253,11 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		*
 		* @method buyDevCard
 		*/
-		ClientModel.prototype.buyDevCard = function() {}
+		ClientModel.prototype.buyDevCard = function() {
+			//1 Confirm the that player has the right resources
+			//2 Reduce the player's resources
+			//3 Update the player's hand with the new dev card
+		}
 
 
 		/**
@@ -231,7 +272,13 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* @param {ResourceList} offerList list of resources being offered
 		* @param {ResourceList} requestList list of resources being requested
 		*/
-		ClientModel.prototype.offerTrade = function(playerIndex, offerList, requestList) {}
+		ClientModel.prototype.offerTrade = function(playerIndex, offerList, requestList) {
+			//1 Confirm the that the player has the appropriate resources
+			//2 Query the other player with the trade offer
+			//3 Wait for response
+			//4 Reduce the player's resources by the offer list 
+			//5 Increase the player's resources by the request list
+		}
 
 		/**
 		* Trade a resource with the bank
@@ -244,7 +291,11 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* @param {ResourceList} offerList list of resources being offered
 		* @param {ResourceList} requestList list of resources being requested
 		*/
-		ClientModel.prototype.tradeWithBank = function(offerList, requestList) {}
+		ClientModel.prototype.tradeWithBank = function(offerList, requestList) {
+			//1 Confirm the that the player has the appropriate resources
+			//2 Reduce the player's resources by the offer list 
+			//3 Increase the player's resources by the request list
+		}
 
 		/**
 		* Use a development card from player's hand
@@ -254,9 +305,11 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* </pre>
 		* 
 		* @method playDevCard
-		* @param {DevCardList} cardToPlay card that will be played from the player's hand
+		* @param {string} cardToPlay card that will be played from the player's hand
 		*/
-		ClientModel.prototype.useDevCard = function(cardToPlay) {}
+		ClientModel.prototype.useDevCard = function(cardToPlay) {
+			//1 Use the dev card specifed
+		}
 
 		/**
 		* Move the robber to the specified hexgrid location
@@ -266,9 +319,12 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* </pre>
 		* 
 		* @method robberMove
-		* @param {HexLocation} locationToMove location to move the robber to
+		* @param {Hex} locationToMove location to move the robber to
 		*/
-		ClientModel.prototype.robberMove = function(locationToMove) {}
+		ClientModel.prototype.robberMove = function(locationToMove) {
+			//1 Remove the robber from the previous location
+			//2 move it to the new location
+		}
 
 		/**
 		* Steal resources with the robber
@@ -281,7 +337,12 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* 
 		* @method steal
 		*/
-		ClientModel.prototype.steal = function() {}
+		ClientModel.prototype.steal = function() {
+			//1 Allow the player to select another player
+			//2 Allow the player to select resource to steal
+			//3 Reduce the resources in the stolen players's hand
+			//4 Increase the resources in the current player's hand
+		}
 
 		/**
 		* Listens for who has the largest army
@@ -292,7 +353,10 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		*
 		* @method largestArmy
 		*/
-		ClientModel.prototype.largestArmy = function() {}
+		ClientModel.prototype.largestArmy = function() {
+			//1 Reduce the victory points of the last owner
+			//2 Increase the victory points of the current player
+		}
 
 		/**
 		* Listens for who has the longest road
@@ -303,7 +367,10 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		*
 		* @method longestRoad
 		*/
-		ClientModel.prototype.longestRoad = function() {}
+		ClientModel.prototype.longestRoad = function() {
+			//1 Reduce the victory points of the last owner
+			//2 Increase the victory points of the current player
+		}
 
 		/**
 		* Declare a winner and end the game
@@ -314,7 +381,10 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		*
 		* @method endGame
 		*/
-		ClientModel.prototype.endGame = function() {}
+		ClientModel.prototype.endGame = function() {
+			//1 Stop turn progression
+			//2 Display end of game
+		}
 
 		/**
 		* End the current player's turn and allow the next player to move
@@ -325,7 +395,9 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		*
 		* @method finishTurn
 		*/
-		ClientModel.prototype.finishTurn = function() {}
+		ClientModel.prototype.finishTurn = function() {
+			//1 Increment turn tracker
+		}
 
 
 
@@ -336,7 +408,9 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* post-conditions: Permission to buy a road is returned
 		* </pre>
 		*/
-		ClientModel.prototype.canBuildRoad = function() {}
+		ClientModel.prototype.canBuildRoad = function() {
+			//1 Check player's resources
+		}
 
 		/**
 		* Determine if the player has suffcient resources to build a settlement
@@ -345,7 +419,9 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* post-conditions: Permission to buy a settlement is returned
 		* </pre>
 		*/
-		ClientModel.prototype.canBuildSettlement = function() {}
+		ClientModel.prototype.canBuildSettlement = function() {
+			//1 Check player's resources
+		}
 
 		/**
 		* Determine if the player has suffcient resources to build a city
@@ -354,7 +430,9 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* post-conditions: Permission to buy a city is returned
 		* </pre>
 		*/
-		ClientModel.prototype.canBuildCity = function() {}
+		ClientModel.prototype.canBuildCity = function() {
+			//1 Check player's resources
+		}
 
 		/**
 		* Determine if the player has suffcient resources to buy a development card
@@ -363,18 +441,20 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* post-conditions: Permission to buy a development card is returned
 		* </pre>
 		*/
-		ClientModel.prototype.canBuyDevCard = function() {}
+		ClientModel.prototype.canBuyDevCard = function() {
+			//1 Check player's resources
+		}
 
 		/**
-		* List out the resource cards owned by the specified player
+		* List out the resource cards owned by the current player
 		* <pre>
 		* pre-conditions: NONE
 		* post-conditions: List of resource cards for the specified player
 		* </pre>
-		*
-		* @param {int} playerIndex index of the player for which to list resoucres
 		*/
-		ClientModel.prototype.listPlayerResources = function(playerIndex) {}
+		ClientModel.prototype.currentPlayerResources = function() {
+			//1 Get player's resources
+		}
 
 		/**
 		* Determine if the player has suffcient resources to offer a given trade
@@ -383,7 +463,9 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* post-conditions: Permission to offer a trade is returned
 		* </pre>
 		*/
-		ClientModel.prototype.canOfferTrade = function() {}
+		ClientModel.prototype.canOfferTrade = function() {
+			//1 Check player's resources
+		}
 
 		/**
 		* Determine if the player has seven resource cards and must discard due to the roll of a seven
@@ -392,7 +474,9 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* post-conditions: Order to discard cards or not 
 		* </pre>
 		*/
-		ClientModel.prototype.needToDiscardCheck = function() {}
+		ClientModel.prototype.needToDiscardCheck = function() {
+			//1 Check player's resources
+		}
 
 		/**
 		* Determine if the player has suffcient resources to accept a given trade
@@ -401,7 +485,9 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* post-conditions: Permission to accept a trade is returned
 		* </pre>
 		*/
-		ClientModel.prototype.canAcceptTrade = function() {}
+		ClientModel.prototype.canAcceptTrade = function() {
+			//1 Check player's resources
+		}
 
 		/**
 		* Determine if the map will allow the player to build a road at the given edge
@@ -409,8 +495,15 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* pre-conditions: It is the current player's turn
 		* post-conditions: Permission to build a road is returned
 		* </pre>
+		*
+		* @param {Hex} hex Hex where the edge is based from
+		* @param {string} edgeDirection the direction the edge is at from the hex
 		*/
-		ClientModel.prototype.canPlaceRoad = function() {}
+		ClientModel.prototype.canPlaceRoad = function(hex, edgeDirection) {
+			//1 Check if there is a road on the edge
+			//2 Check if there is a city or settlement owned by player on the two vertexes
+			//3 Check if there is a road on the four edges that connect
+		}
 
 		/**
 		* Determine if the map will allow the player to build a settlement at the given vertex
@@ -418,8 +511,29 @@ catan.models.ClientModel  = (function clientModelNameSpace(){
 		* pre-conditions: It is the current player's turn
 		* post-conditions: Permission to build a settlement is returned
 		* </pre>
+		*
+		* @param {Hex} hex Hex where the vertex is based from
+		* @param {string} vertDirection the direction the vertex is at from the hex
 		*/
-		ClientModel.prototype.canPlaceSettlement = function() {}
+		ClientModel.prototype.canPlaceSettlement = function(hex, vertDirection) {
+			//1 Check if there is a settlement or city on the vertex
+			//2 Check if there is a road on the edges that connect
+			//3 Check if there is a settlement or city on the vertexes that are connected to the connected edges
+		}
+
+		/**
+		* Determine if the map will allow the player to build a city at the given vertex
+		* <pre>
+		* pre-conditions: It is the current player's turn
+		* post-conditions: Permission to build a city is returned
+		* </pre>
+		*
+		* @param {Hex} hex Hex where the vertex is based from
+		* @param {string} vertDirection the direction the vertex is at from the hex
+		*/
+		ClientModel.prototype.canPlaceCity = function(hex, vertDirection) {
+			//1 Check if there is a settlement on the vertex owned by the player
+		}
         
 		return ClientModel;
 	}());	
