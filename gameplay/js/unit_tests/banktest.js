312e5387e569d8ceb965a6f3af222e7bcab2f2c6
discodane <el.dane.erickson@gmail.com>
2014-02-07 12:11:06 -0700

    quick

--- gameplay/js/unit_tests/banktest.js
+++ gameplay/js/unit_tests/banktest.js
@@ -1,5 +1,15 @@
-test("dev Card something", function(){
+test("Year of Plenty", function(){
 	var namespace = catan.model.bank();
-	var dev = namspace.DevCard(catan.model);
-	
+	// var dev = namspace.DevCard(catan.model);
+	// parameter = new Object();
+	// parameter.resource1="brick";
+	// parameter.resource2="ore";
+	// parameter.playerIndex=1;
+	// var beforeCount = catan.model.bank.resourceList.brick;
+	// var beforeCountO = catan.model.bank.resourceList.ore;
+	// dev.useCard("yearOfPlenty", parameter);
+	// var afterCount = catan.model.bank.resourceList.brick;
+	// var afterCount0 = catan.model.bank.resourceList.ore;
+
+	// ok(beforeCount>afterCount, "Unable to year of plenty");
 });
