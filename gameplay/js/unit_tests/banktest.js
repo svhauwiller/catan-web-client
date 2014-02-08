5bf88cece8e99e8d618145b5e63af5459af9cc63
discodane <el.dane.erickson@gmail.com>
2014-02-07 23:56:50 -0700

    Finished up testing for dev cards

--- gameplay/js/unit_tests/banktest.js
+++ gameplay/js/unit_tests/banktest.js
@@ -34,6 +34,52 @@ test("Monument", function(){
 
 	dev.useCard("monument", parameter);
 
-	var vicAfter = model.player[parameter.playerIndex].victoryPts;
+	var vicAfter = model.players[parameter.playerIndex].victoryPts;
 	ok(vicAfter>vicBefore, "Unable to monument");
+});
+
+test("Monopoly", function(){
+	
+	var namespace = catan.models.bank;
+	var model = new catan.models.ClientModel(0);
+	var dev = new namespace.DevCard(model);
+	var parameter = new Object();
+	parameter.playerIndex=0;
+	parameter.resource="brick";
+
+	dev.useCard("monopoly", parameter);
+
+	var playerRec = model.players[parameter.playerIndex].resources[parameter.resource];
+	var bankRec = model.bank.resourceList[parameter.resource];
+	ok(playerRec + bankRec === 19, "Unable to monopoly");
+});
+
+test("RoadBuilder", function(){
+	
+	var namespace = catan.models.bank;
+	var model = new catan.models.ClientModel(0);
+	var dev = new namespace.DevCard(model);
+	var parameter = new Object();
+	parameter.playerIndex=0;
+	var hex = new Object();
+	hex.x = 1;
+	hex.y = 2;
+	parameter.hex=hex;
+	parameter.direction="NW";
+
+	var before = model.players[parameter.playerIndex].roads;
+
+	dev.useCard("roadBuilding", parameter);
+
+	var after = model.players[parameter.playerIndex].roads;
+
+	ok(before>after, "Unable to roadbuild");
+});
+
+test("Soldier", function(){
+	
+	var namespace = catan.models.bank;
+	var model = new catan.models.ClientModel(0);
+	var dev = new namespace.DevCard(model);
+	var parameter = new Object();
 });
