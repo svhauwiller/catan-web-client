b8bf27122d4c8b0e7e521dabe3c31ced134b6d45
Jonathan JrChi Tsai <jtsai01@gmail.com>
2014-02-24 23:43:20 -0700

    lala land

--- gameplay/js/model/hexgrid_impl.js
+++ gameplay/js/model/hexgrid_impl.js
@@ -130,7 +130,9 @@ catan.models.Map = (function mapNameSpace(){
 		}
 	
 		Map.prototype.canPlaceRoad = function(playerID, hex, theDirection){
+			/*console.log(hex);
 			var edgeDirection = hex.getEdge(hexgrid.EdgeDirection[theDirection]);
+			console.log(edgeDirection);
 			var touchingHexes = edgeDirection.getLocation().getEquivalenceGroup();
 			//settlement owned by player on a vertex touching the road position
 			console.log(hex.getVertex(hexgrid.VertexDirection[theDirection]));
@@ -150,23 +152,12 @@ catan.models.Map = (function mapNameSpace(){
 				theTempHex.getEdge(hexgrid.nextDirectionCounterClockwise(touchingHexes[equivE].direction)).getOwner()===playerID){
 					return true;
 				}		      
-			}
-			/*
-	
-	//Works on Hex, Edge and Vertex Directions
-	function nextDirectionClockwise(direction){
-		return positiveModulo((direction + 1),6);
-	}
-	
-	//Works on Hex, Edge and Vertex Directions
-	function nextDirectionCounterClockwise(direction){
-		return positiveModulo((direction - 1),6);			
-			*/			
-			
+			}*/
+			return true;
 		};
 		
 		Map.prototype.canPlaceSettlement = function(playerID, hex, theDirection){
-			var vertexDirection = hex.getVertex(theDirection);
+			/*var vertexDirection = hex.getVertex(theDirection);
 			var touchingHexes = vertexDirection.getEquivalenceGroup();
 			
 			//slot unoccupied
@@ -187,7 +178,8 @@ catan.models.Map = (function mapNameSpace(){
 					return true;
 					}
 			}
-			return false;
+			return false;*/
+			return true;
 		};
 		
 		Map.prototype.canPlaceCity = function(playerID, hex, theDirection){
