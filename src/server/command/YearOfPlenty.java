16eb424920b527d3382454363ffbd9ef0c655921
discodane <el.dane.erickson@gmail.com>
2014-03-18 19:40:04 -0600

    Monopoly and year of plenty commands

--- src/server/command/YearOfPlenty.java
+++ src/server/command/YearOfPlenty.java
@@ -8,14 +8,12 @@ public class YearOfPlenty implements CommandTemplate {
 		//MOSTLY SUDO CODE
 		//Will need to figure out the parameters because they will contain what resource needs to be taken
 
-		//Take card away from current player
+		//Take dev card away from current player
 		GameModel.getCurrentPlayer().getOldDevCards().updateYearOfPlenty(-1);
 		
 		//take resources away from gamemodel bank
-		//here we will refer to the 2 resources both as the "args[i]" given
-		// when we change the parameter to a String[] then you can just as easy say args[i][x]
 		for(int i=0; i<2;i++){
-			if(args[i][i].compareTo("wheat")==0)
+			if(args[i].compareTo("wheat")==0)
 				GameModel.getBank().updateWheat(-1);
 			else if(args[i].compareTo("ore")==0)
 				GameModel.getBank().updateOre(-1);
