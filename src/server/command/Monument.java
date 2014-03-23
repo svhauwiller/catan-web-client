0850054a48b4c931cd90649e1c79c62ca2b2e0a0
discodane <el.dane.erickson@gmail.com>
2014-03-22 19:21:50 -0600

    monument done

--- src/server/command/Monument.java
+++ src/server/command/Monument.java
@@ -4,8 +4,16 @@ import server.communication.GameModel;
 import java.util.*;
 
 public class Monument implements CommandTemplate {
+	//args[0] = player id
 	public GameModel execute(String[] args){
-	return null;
+		int playerNum = Integer.parseInt(args[0]);
+		GameModel gmod = GameModel.getInstance();
+		//change the points
+
+		gmod.getPlayer(playerNum).updateVictoryPoints(1);
+		gmod.getPlayer(playerNum).getOldDevCards().updateMonument(-1);
+		gmod.getPlayer(playerNum).updateMonuments(1);
+		return gmod;
 	}
 	public void undo(){}
 }
