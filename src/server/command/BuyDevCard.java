be566772cc1c30839cc10f43a05d075c45c50cfb
discodane <el.dane.erickson@gmail.com>
2014-03-24 21:22:53 -0600

    fixed undo in devcard

--- src/server/command/BuyDevCard.java
+++ src/server/command/BuyDevCard.java
@@ -5,9 +5,11 @@ import java.util.*;
 
 public class BuyDevCard implements CommandTemplate{
 
+	private int x= -1;
+	private int playerNum=-1;
 	//args[0] = Playerid
 	public GameModel execute(String[] args){
-		int playerNum = Integer.parseInt(args[0]);
+		playerNum = Integer.parseInt(args[0]);
 		//This is mostly sudo code and will need to be changed in accordance with the actual methods that have yet to be made
 		//And because im not 100% sure what the args are
 
@@ -24,7 +26,7 @@ public class BuyDevCard implements CommandTemplate{
 		
 		//retrieve the dev card
 		Random rand = new Random();
-		int x = rand.nextInt(5);
+		x = rand.nextInt(5);
 		
 		//it could be that the random number is already done and that the args is which dev card to give.
 		// that would simplify things but in the case that random happens here, we already have it done
@@ -57,46 +59,47 @@ public class BuyDevCard implements CommandTemplate{
 	}
 	
 	public void undo(){
-		/*GameModel.bank.updateOre(-1);
-		GameModel.bank.updateWheat(-1);
-		GameModel.bank.updateSheep(-1);
+		//This is mostly sudo code and will need to be changed in accordance with the actual methods that have yet to be made
+		//And because im not 100% sure what the args are
+
+		//put the resources back in the bank
+		//This isn't possible because bank is private;
+		GameModel.getBank().updateOre(-1);
+		GameModel.getBank().updateWheat(-1);
+		GameModel.getBank().updateSheep(-1);
 		
 		//take those resources away from Player
-		Player player = GameModel.getCurrentPlayer();
-		ResourceCardList myResource = player.getResources();
-		myResource.updateOre(1);
-		myResource.updateWheat(1);
-		myResource.updateSheep(1);
-
+		GameModel.getPlayer(playerNum).getResourceCardList().updateOre(1);
+		GameModel.getPlayer(playerNum).getResourceCardList().updateWheat(1);
+		GameModel.getPlayer(playerNum).getResourceCardList().updateSheep(1);
+		
 		//retrieve the dev card
-		DevCardList myDevList = GameModel.getDeck(); //This still needs to be made
-		Random rand = new Random();
-		int x = rand.nextInt(5);
-		//Not sure how to do this since it is done randomly 
+		//it could be that the random number is already done and that the args is which dev card to give.
+		// that would simplify things but in the case that random happens here, we already have it done
 		if(x == 0){
 			//be yearofPlenty
-			player.newDevCards.updateYearOfPlenty(-1);
-			myDevList.updateYearOfPlenty(1);
+			GameModel.getPlayer(playerNum).getNewDevCards().updateYearOfPlenty(-1);
+			GameModel.getDeck().updateYearOfPlenty(1);
 		}
 		else if(x == 1){
 			//be Monopoly
-			player.newDevCards.updateMonopoly(-1);
-			myDevList.updateMonopoly(1);
+			GameModel.getPlayer(playerNum).getNewDevCards().updateMonopoly(-1);
+			GameModel.getDeck().updateMonopoly(1);
 		}
 		else if(x==2){
 			//be Soldier
-			player.newDevCards.updateSoldier(-1);
-			myDevList.updateSolider(1);
+			GameModel.getPlayer(playerNum).getNewDevCards().updateSoldier(-1);
+			GameModel.getDeck().updateSoldier(1);
 		}
 		else if(x==3){
 			//be RoadBuilder
-			player.newDevCards.updateRoadBuilder(-1);
-			myDevList.updateRoadBuilder(1);
+			GameModel.getPlayer(playerNum).getNewDevCards().updateRoadBuilding(-1);
+			GameModel.getDeck().updateRoadBuilding(1);
 		}
 		else if(x==4){
 			//be monument
-			player.oldDevCards.updateMonument(-1);
-			myDevList.updateMonument(1);
-		}*/
+			GameModel.getPlayer(playerNum).getNewDevCards().updateMonument(-1);
+			GameModel.getDeck().updateMonument(1);
+		}
 	}
 }
