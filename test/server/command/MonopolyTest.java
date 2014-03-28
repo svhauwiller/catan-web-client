ac0638d178b91de37e231e12f28afc1c94e7f224
discodane <el.dane.erickson@gmail.com>
2014-03-28 12:26:20 -0600

    More Test!!! because thats all i can do

--- test/server/command/MonopolyTest.java
+++ test/server/command/MonopolyTest.java
@@ -16,12 +16,17 @@ import server.api.player.Player;
 import server.command.*;
 import server.api.player.Player.PlayerColor;
 
-public class MonumentTest{
-	public Monument monObject = new Monument();
+public class MonopolyTest{
+	public Monopoly mObject = new Monopoly();
+	String[] args = new String[2];
 	@Before
 	public void setup(){
 		GameModel.addPlayer(new Player(0, new PlayerInfo(PlayerColor.orange, 0, "Sam")));
-		GameModel.getPlayer(0).getOldDevCards().updateMonument(1);
+		GameModel.addPlayer(new Player(1, new PlayerInfo(PlayerColor.red, 1, "Brooke")));
+		GameModel.addPlayer(new Player(2, new PlayerInfo(PlayerColor.blue, 2, "Joe")));
+		GameModel.getPlayer(0).getOldDevCards().updateMonopoly(1);
+		GameModel.getPlayer(1).getResourceCardList().updateWood(3);
+		GameModel.getPlayer(2).getResourceCardList().updateWood(2);
 	}
 
 	@Test
@@ -29,10 +34,13 @@ public class MonumentTest{
 		
 		//assertEquals(GameModel.getPlayer(1).getOldDevCards(),0);
 		
-		String[] args = new String[]{"0"};
-		monObject.execute(args);
-		assertEquals(GameModel.getPlayer(0).getOldDevCards().getTotal(), 0);
-		assertEquals(GameModel.getPlayer(0).getVictoryPoints(), 1);
+		args[0] = "0";
+		args[1] = "wood";
+		mObject.execute(args);
+		assertEquals(GameModel.getPlayer(0).getOldDevCards().getTotal() + GameModel.getPlayer(0).getNewDevCards().getTotal(), 0);
+		assertEquals(GameModel.getPlayer(0).getResourceCardList().getWood(),5);
+		assertEquals(GameModel.getPlayer(1).getResourceCardList().getWood(),0);
+		assertEquals(GameModel.getPlayer(2).getResourceCardList().getWood(),0);
 	}
 
 	// @Test
