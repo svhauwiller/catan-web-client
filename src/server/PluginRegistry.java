03e19372d39734c7b4162ea0f30c07226ac58d2e
whauwiller <whauwiller@gmail.com>
2014-04-09 07:04:32 -0600

    Plugin Stuff

--- src/server/PluginRegistry.java
+++ src/server/PluginRegistry.java
@@ -13,6 +13,26 @@ import java.util.ArrayList;
  * @author Wesley
  */
 public class PluginRegistry {
-	ArrayList<Object> availablePlugins;
+	private static PluginRegistry instance;
+	private static PluginRegistry instance(){
+		if(instance == null){
+			instance = new PluginRegistry();
+		}
+		return instance;
+	}
 	
+	public ArrayList<Object> availablePlugins;
+	
+	private PluginRegistry(){
+		availablePlugins = new ArrayList<>();
+	}
+	
+	private void _registerPlugin(Object plugin){
+		availablePlugins.add(plugin);
+	}
+	
+	private ArrayList<Object> _getAvailablePlugins(){
+		return availablePlugins;
+	}
+
 }
